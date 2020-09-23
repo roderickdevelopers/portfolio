6b5fc23e85b52b5a6ed2b9005278667e55ae5ef6
Matthew <roderickdevelopers@gmail.com>
2020-09-22 20:34:24 -0400

    home for my projects is complete, also added small skills section

---
+++ app/components/structural/routes/home-index/component.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class StructuralRoutesHomeIndex extends Component {
+  imageFrontCardThree = '/assets/images/html.png';
+  imageFrontCardFour = '/assets/images/css.png';
+  imageFrontCardFive = '/assets/images/javascript.png';
+  imageFrontCardSix = '/assets/images/githubfront.png';
+  imageFrontCardSeven = '/assets/images/ember.png';
+  imageFrontCardEight = '/assets/images/react.png';
+  imageFrontCardNine = '/assets/images/studiocode.png';
+  imageFrontCardTen = '/assets/images/sketch.png';
+}
---
+++ app/components/structural/routes/projects/index/component.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+
+export default class StructuralRoutesProjectsIndexComponent extends Component {
+  imageFrontCardOne = '/assets/images/markdownfront.png';
+  imageFrontCardTwo = '/assets/images/sleepyforestfront.png';
+}
