8cf7496059094ef261e1ebbc3f54f9862c0d6cb5
Matthew <roderickdevelopers@gmail.com>
2020-11-03 09:35:03 -0500

    forgot to make new branch

--- app/components/structural/routes/projects/index/component.js
+++ app/components/structural/routes/projects/index/component.js
@@ -3,4 +3,5 @@ import Component from '@glimmer/component';
 export default class StructuralRoutesProjectsIndexComponent extends Component {
   imageFrontCardOne = '/assets/images/markdownfront.png';
   imageFrontCardTwo = '/assets/images/sleepyforestfront.png';
+  siteLabel = 'Site';
 }
