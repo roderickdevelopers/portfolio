d4b1bea70603b8b4029fe325bf16491599f399ee
Matthew <roderickdevelopers@gmail.com>
2020-12-28 15:42:42 -0500

    Took off live site in projects when not needed and added target blank to links

--- app/components/structural/routes/contact/component.js
+++ app/components/structural/routes/contact/component.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 
 export default class StructuralRoutesContactComponent extends Component {
-  paragraphOne = 'Email with any questions or inquiries. I would be happy to answer any questions and set up a meeting with you!  ';
+  paragraphOne = 'Email with any questions or inquiries. I would be happy to set up a meeting with you!';
 }
 
 
