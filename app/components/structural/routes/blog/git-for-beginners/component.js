45b98df588e198c4ca6f7d649ffa3f5ef1165dc9
Matthew <roderickdevelopers@gmail.com>
2021-02-25 12:09:53 -0500

    Desktop navigation
    Added a desktop navigation, added a back to top button with fade in, changed projects page to grid.

--- app/components/structural/routes/blog/git-for-beginners/component.js
+++ app/components/structural/routes/blog/git-for-beginners/component.js
@@ -1,5 +1,14 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class StructuralRoutesGitForBeginnersRouteComponent extends Component {
-  
+  @action
+  backToTop() {
+    // console.log("You just fired the back to top action when you clicked back to top button");
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
 }
--- app/components/structural/routes/blog/index/component.js
+++ app/components/structural/routes/blog/index/component.js
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component';
 
 export default class StructuralRoutesBlogIndexComponent extends Component {
+  paragraphOne = "Some text here"
   imageFrontCardOne = 'https://roderickdevelopers.s3.us-east-2.amazonaws.com/embermobilemenu.webp';
+  imageFrontCardOneSmall = '/assets/files/gitblogsmall.webp';
   imageFrontFallBack = 'https://roderickdevelopers.s3.us-east-2.amazonaws.com/embermobilemenu.png'
   imageFrontCardTwo = 'https://roderickdevelopers.s3.us-east-2.amazonaws.com/gitforbeginners.webp';
   imageFrontFallBackTwo = 'https://roderickdevelopers.s3.us-east-2.amazonaws.com/gitforbeginners.png'
